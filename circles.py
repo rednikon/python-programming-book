400410a36800f2f235c91473ce0f04b3f07be07c
Vee McClure <rednikon@yahoo.com>
2021-04-23 20:28:43 -0700

    completed exercise

---
+++ circles.py
@@ -0,0 +1,11 @@
+# pg. 74 Mathematical expressions
+
+import math
+
+def get_volume():
+    radius = int(input("Let's calculate the volume and area of a circle. Please enter a value for the radius: "))
+    volume = 4 / 3 * (math.pi * radius**3)
+    area = 4 * math.pi * radius**2
+    print("The circle's volume is: ", round(volume, 2), "and the circle's area is: ", round(area, 2))
+
+get_volume()
